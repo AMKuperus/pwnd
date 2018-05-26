bc100d52adf8432341dffc8af9122c8a5b765401
AMKuperus <jezeewolde@gmail.com>
2018-05-26 21:38:29 +0200

    Removed passwordfields and erase history when using multiple times

--- passwordHandlers.go
+++ passwordHandlers.go
@@ -10,9 +10,9 @@ import (
 )
 
 type Answer struct {
-	Password string `json:"password"`
-	Found    bool   `json:"found"`
-	Value    int    `json:"value"`
+	//Password string `json:"password"`
+	Found bool `json:"found"`
+	Value int  `json:"value"`
 }
 
 var answers []Answer
@@ -28,8 +28,11 @@ func getPasswordAnswerHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func checkPasswordHandler(w http.ResponseWriter, r *http.Request) {
+	// Reset answers to keep 1 answer and delete the history.
+	if len(answers) > 0 {
+		answers = nil
+	}
 	pass := pwnd.Password{}
-
 	err := r.ParseForm()
 	if err != nil {
 		log.Printf("Error: %v", err)
@@ -46,16 +49,20 @@ func checkPasswordHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	if !pass.Found() {
 		// Found nothing
-		log.Printf("Found nothing for %s\n", pass.Word)
+		answer := Answer{}
+		answer.Found = pass.Found()
+		answers = append(answers, answer)
+		log.Printf("%s\n", color.GreenString("Found nothing | End of this request."))
+	} else {
+		answer := Answer{}
+		//answer.Password = pass.Word
+		answer.Found = pass.Found()
+		answer.Value = pass.Value()
+		answers = append(answers, answer)
+		//log.Printf("Found %s - %d\n", color.CyanString(pass.Word), pass.Value())
+		//log.Printf("%#v", answer)
+		log.Printf("%s\n", color.GreenString("Found the answer | End of this request."))
 	}
-
-	answer := Answer{}
-	answer.Password = pass.Word
-	answer.Found = pass.Found()
-	answer.Value = pass.Value()
-	answers = append(answers, answer)
-	//log.Printf("Found %s - %d\n", color.CyanString(pass.Word), pass.Value())
-	//log.Printf("%#v", answer)
-	log.Printf("%s\n", color.GreenString("Found the answer | End of this request."))
+	log.Printf("%#v\n", answers)
 	http.Redirect(w, r, "/assets/password.html", http.StatusFound)
 }
